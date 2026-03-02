85ad72839a08fecfdd96f7fc1c6b3bd57ff5456f
Muflay <muflihattamirmuslim@gmail.com>
2026-03-02 16:23:56 +0700

    mengubah r menjadi jarijari

--- ChallangeTime.cpp
+++ ChallangeTime.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 float phi = 3.14159;
-int r;
+int jarijari;
 
 void input (){  
     cout << "Masukkan Ruas = ";
-    cin >> r;
+    cin >> jarijari;
 }
 
 float luas_lingkaran(int c) {
     return phi * c * c;
-}}  
+}  
 
 void output() {
-    cout << "Hasil luas Lingkaran = " << luas_lingkaran(r) << endl;
+    cout << "Hasil luas Lingkaran = " << luas_lingkaran(jarijari) << endl;
 }
 int main() {
     input();
